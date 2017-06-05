8bc1a555e3dcde0fedb5c94ef0d6721f2bbda066
Devan Moylan <dmmoylan@gmail.com>
2017-06-05 22:32:38 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,41 @@
+INSERT INTO series(title, author_id, subgenre_id) VALUES ("BOBS SHED", 1, 1);
+INSERT INTO series(title, author_id, subgenre_id) VALUES ("BILLS SHED", 1, 1);
+
+INSERT INTO books(title, year, series_id) VALUES ("TIMS SHED", 2017, 1);
+INSERT INTO books(title, year, series_id) VALUES ("TIMS SHED", 2017, 1);
+INSERT INTO books(title, year, series_id) VALUES ("TIMS SHED", 2017, 1);
+INSERT INTO books(title, year, series_id) VALUES ("TIMS SHED", 2017, 1);
+INSERT INTO books(title, year, series_id) VALUES ("TIMS SHED", 2017, 1);
+INSERT INTO books(title, year, series_id) VALUES ("TIMS SHED", 2017, 1);
+
+INSERT INTO characters(name, motto) VALUES ("TIM", "THE SHED BELONGS TO ME");
+INSERT INTO characters(name, motto) VALUES ("JAKE", "THE shed belongs to tim");
+INSERT INTO characters(name, motto) VALUES ("AIMEE", "The shed belongs to TIM?");
+INSERT INTO characters(name, motto) VALUES ("AIMEE", "The shed belongs to TIM?");
+INSERT INTO characters(name, motto) VALUES ("AIMEE", "The shed belongs to TIM?");
+INSERT INTO characters(name, motto) VALUES ("AIMEE", "The shed belongs to TIM?");
+INSERT INTO characters(name, motto) VALUES ("AIMEE", "The shed belongs to TIM?");
+INSERT INTO characters(name, motto) VALUES ("AIMEE", "The shed belongs to TIM?");
+
+INSERT INTO subgenres(name) VALUES ("TIN ROOFED");
+INSERT INTO subgenres(name) VALUES ("Thatched");
+
+INSERT INTO authors(name) VALUES ("The groundhog");
+INSERT INTO authors(name) VALUES ("Long and TALL");
+
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
--- lib/querying.rb
+++ lib/querying.rb
@@ -1,17 +1,70 @@
 def select_books_titles_and_years_in_first_series_order_by_year
+  "
+  SELECT books.title, books.year
+  FROM books
+  INNER JOIN series
+  ON books.series_id = series.id
+  GROUP BY books.title
+  HAVING books.series_id = 1
+  ORDER BY books.year;
+  "
 end
 
 def select_name_and_motto_of_char_with_longest_motto
+  "
+  SELECT name, motto
+  FROM characters
+  ORDER BY LENGTH(motto) DESC
+  LIMIT 1;
+  "
 end
 
 def select_value_and_count_of_most_prolific_species
+  "
+  SELECT characters.species, COUNT(characters.species)
+  FROM characters
+  GROUP BY characters.species
+  ORDER BY COUNT(characters.species) DESC
+  LIMIT 1
+  ;
+  "
 end
 
 def select_name_and_series_subgenres_of_authors
+  "
+  SELECT authors.name, subgenres.name
+  FROM authors
+  JOIN series
+  ON authors.id = series.author_id
+  JOIN subgenres
+  ON series.subgenre_id = subgenres.id
+  GROUP BY authors.name
+  ;
+  "
 end
 
 def select_series_title_with_most_human_characters
+  "
+  SELECT series.title
+  FROM series
+  JOIN characters
+  ON series.id = characters.series_id
+  GROUP BY characters.series_id
+  ORDER BY COUNT(characters.series_id) DESC
+  LIMIT 1
+  ;
+  "
 end
 
+#The return value will be the characters name and the count of books they appear in. The models need will be the characters joined with the books. There is a model containing both the books id's and the characters, which would likely be the best place to start.
 def select_character_names_and_number_of_books_they_are_in
+  "
+  SELECT characters.name, COUNT(character_books.character_id)
+  FROM characters
+  JOIN character_books
+  ON characters.id = character_books.character_id
+  GROUP BY character_books.character_id
+  ORDER BY COUNT(character_books.character_id) DESC, characters.name
+  ;
+  "
 end
